659a452ec01ab935342af65933207ca6390f36a6
Pooja <Pooja@192.168.0.100>
2017-10-31 22:59:15 -0700

    find political donors commit push

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 #python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
-javac "/Users/keerthivasan86/git/find-political-donors/src/findPoliticalDonors/FindPoliticalDonors.java"
-#java -classpath "/Users/keerthivasan86/git/find-political-donors/src/" findPoliticalDonors.FindPoliticalDonors /Users/keerthivasan86/git/find-political-donors/insight_testsuite/tests/test_1/input/itcont.txt /Users/keerthivasan86/git/find-political-donors/insight_testsuite/tests/test_1/output
+javac "/Users/git/find-political-donors/src/findPoliticalDonors/FindPoliticalDonors.java"
+java -classpath "/Users/git/find-political-donors/src/" findPoliticalDonors.FindPoliticalDonors /Users/keerthivasan86/git/find-political-donors/insight_testsuite/tests/test_1/input/itcont.txt /Users/keerthivasan86/git/find-political-donors/insight_testsuite/tests/test_1/output
 
 
--- src/README.md
+++ src/README.md
@@ -1,1 +1,12 @@
 This is the directory where your source code would reside.
+
+Approach was to use a hashmap and calculate the medians of the donors based on zipcode and date.
+
+to run the java file, please use run.sh and need to give 2 parameters as input:
+param1: input file
+param2: output folder location.
+
+Eg:
+javac "/Users/keerthivasan86/git/find-political-donors/src/findPoliticalDonors/FindPoliticalDonors.java"
+
+java -cp /Users/git/find-political-donors/src/ findPoliticalDonors.FindPoliticalDonors /Users/git/find-political-donors/insight_testsuite/tests/test_1/input/itcont.txt /Users/git/find-political-donors/insight_testsuite/tests/test_1/output
